3e2bc2a985facd65ddcccc4ce850417eca64b488
Leo <935987+zeropointcoder@users.noreply.github.com>
2025-11-05 15:01:37 +0000

    Update import

--- qiskit/shor-factorisation/shor_factorisation.py
+++ qiskit/shor-factorisation/shor_factorisation.py
@@ -1,16 +1,1 @@
-from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
-from qiskit_algorithms import Shor
-from qiskit.utils import QuantumInstance
-
-N = 15 # choose a small number only 
-
-simulator = AerSimulator() # simulate
-
-qi = QuantumInstance(backend=simulator) # create a QuantumInstance for the simulator
-
-shor = Shor(QuantumInstance=qi) # initialise Shor's algorithm
-
-result = shor.factor(N) # run the algorithm
-
-print(f"Factors of {N}: {result.factors}")
